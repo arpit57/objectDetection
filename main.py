7c0811628f178038903d77bf84b92f96083b31f1
arpit57 <arpit.singhal57@gmail.com>
2023-06-08 21:17:26 +0530

    added web.config for azure deployment

--- main.py
+++ main.py
@@ -63,6 +63,5 @@ def upload_file():
     return render_template('upload.html')
 
 if __name__ == '__main__':
-    import os
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
